f43565d5f5e0cb0ba5a9b5caa9445d12ee79072e
AnonUD4Y <AnonUD4Y@gmail.com>
2020-05-02 19:18:19 +0530

    Changes For Termux.

--- Defs/Actions.py
+++ Defs/Actions.py
@@ -1044,7 +1044,7 @@ def endMessage(port):  # Message when HiddenEye exit
     choice = input(
         "\n\n{0}[{1}?{0}] Re-run(r) : Exit(x) : Send Email(M) : SelectServer(S)\n\n >> {2}".format(MAIN0, MAIN4, MAIN2)).upper()
     if choice == 'R' or choice == 'r':
-        system('sudo python3 HiddenEye.py')
+        system('python3 HiddenEye.py')
     elif choice == 'M' or choice == 'm':
         emailPrompt3(port)
     elif choice == 'S' or choice == 's':
